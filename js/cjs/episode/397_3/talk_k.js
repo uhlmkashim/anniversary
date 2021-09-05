6ba5a437d4ce15a72b4d909c5ef2074378b4355c
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2021-09-06 03:22:05 +0900

    Create talk_k.js

---
+++ js/cjs/episode/397_3/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "배우는 갖춰졌다…… 지금, 시간이";
+window.im_cjs.t[1][2] = "찼다!…… 하아, 시간에 맞춰서 ";
+window.im_cjs.t[1][3] = "다행이야……! 즉시, 의식을 ";
+window.im_cjs.t[1][4] = "거행하지! 자…… 나와라!";
+
+window.im_cjs.t[2][1] = "어둠으로부터 받은, 나의 지보! 이";
+window.im_cjs.t[2][2] = "순간, 소유권은 친구에게 넘어간다!";
+window.im_cjs.t[2][3] = "(초콜릿을 만들어 왔어요……";
+window.im_cjs.t[2][4] = "받아 주세요!)";
+
+window.im_cjs.t[3][1] = "숨겨둔 힘은 해방되어, 무수한";
+window.im_cjs.t[3][2] = "시신 위에, 창조를 완수했다…….";
+window.im_cjs.t[3][3] = "(잔뜩 연습했으니까,";
+window.im_cjs.t[3][4] = "분명 맛있게 됐다고 생각해)";
+
+window.im_cjs.t[4][1] = "혼돈, 파괴, 그리고, 작열의 업화";
+window.im_cjs.t[4][2] = "…… 나의 친구를 생각하면, 어느 것도";
+window.im_cjs.t[4][3] = "장난일 뿐이다! (기뻐하는 얼굴이";
+window.im_cjs.t[4][4] = "보고 싶어서, 노력했어요!)";
+
+window.im_cjs.t[5][1] = "저기…… 당신에게, 고마운";
+window.im_cjs.t[5][2] = "마음과, 앞으로도 부탁하는";
+window.im_cjs.t[5][3] = "마음을 전하고 싶어서……";
+window.im_cjs.t[5][4] = "직접 만들어 봤어.";
+
+window.im_cjs.t[6][1] = "……으, 아직 업화에 타는";
+window.im_cjs.t[6][2] = "것처럼, 뺨이 뜨거워…… 그러나,";
+window.im_cjs.t[6][3] = "이것으로, 우리들의 맹약은 영원한";
+window.im_cjs.t[6][4] = "것이 됐다! 에헤헤……♪";
+
+window.im_cjs.t[7][1] = "……크흠. 내 친구에게는…… 소중한,";
+window.im_cjs.t[7][2] = "이 말을 보낼게! 해피 발렌타인,";
+window.im_cjs.t[7][3] = "프로듀서……";
+window.im_cjs.t[7][4] = "친애하는 당신에게, 마음을 담아♪";
