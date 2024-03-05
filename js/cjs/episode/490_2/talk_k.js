313e03febbb043ebc1dcde1d47e7a5af26c8695d
uhlmkashim <sagisawa0330@gmail.com>
2024-03-05 21:36:42 +0900

    1

---
+++ js/cjs/episode/490_2/talk_k.js
@@ -0,0 +1,47 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "어떠신가요? 프로듀서 쨔마.";
+window.im_cjs.t[1][2] = "바다가 예쁘죠?";
+window.im_cjs.t[1][3] = "여긴 사쿠라이의 별장 중에서도 손꼽히는";
+window.im_cjs.t[1][4] = "피서지예요♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "희미하게 들리는 파도 소리에";
+window.im_cjs.t[2][2] = "귀를 기울이며 편안하게 쉬거나……";
+window.im_cjs.t[2][3] = "시원한 풀에서 몸을 움직이거나……";
+window.im_cjs.t[2][4] = "뭘 해도 자유예요♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "PC도 휴대전화도,";
+window.im_cjs.t[3][2] = "지금만큼은 금지예요♪";
+window.im_cjs.t[3][3] = "아빠도, 여기에 오면 일을 잊는다고,";
+window.im_cjs.t[3][4] = "자주 말씀하세요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "항상 사람에 둘러싸여,";
+window.im_cjs.t[4][2] = "바쁘게 지내고 계시니까……";
+window.im_cjs.t[4][3] = "쉬는 동안 정도는, 저와 둘이서,";
+window.im_cjs.t[4][4] = "느긋하게 보내요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그래요. 저녁에, 시원해지면 바닷가를";
+window.im_cjs.t[5][2] = "산책할까요♪";
+window.im_cjs.t[5][3] = "프라이빗 비치니까요,";
+window.im_cjs.t[5][4] = "불꽃놀이를 하는 것도 좋겠네요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "어머…… 저 좀 봐, 그만 들떠버렸네요.";
+window.im_cjs.t[6][2] = "프로듀서 쨔마와 하고 싶은 게,";
+window.im_cjs.t[6][3] = "자꾸만 떠올라버려서요.";
+window.im_cjs.t[6][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "프로듀서 쨔마도, 하고 싶은 게";
+window.im_cjs.t[7][2] = "있다면 뭐든 말해주시길.";
+window.im_cjs.t[7][3] = "당신의 억지라면,";
+window.im_cjs.t[7][4] = "저 대환영인걸요♪";
